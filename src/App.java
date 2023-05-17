41aab9f458d0dd83b172a6e0bf6f4680b623fb22
shen-c <tobias.shen@gmail.com>
2023-05-16 17:22:09 -0700

    pushig !

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Hello!");
 
-        System.out.println("Goodbye");
+        System.out.println("Goodbye!");
     }
 }
